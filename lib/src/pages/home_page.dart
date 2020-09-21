22bb654f3f948ba848136d4b6adb87b4cce53f5a
Pardok <jrpardo@gmail.com>
2020-09-21 18:30:37 -0500

    97.1

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -4,7 +4,22 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Hola de nuevo')),
-    );
+        appBar: AppBar(
+          centerTitle: false,
+          backgroundColor: Colors.teal,
+          title: Text("Peliculas en cine"),
+          actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              _swipeTarjetas(),
+            ],
+          ),
+        ));
+  }
+
+  Widget _swipeTarjetas() {
+    return Container();
   }
 }
